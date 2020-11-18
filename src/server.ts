d231448c716b0b066cf022b2eb5777b5dd80fc7a
Hermosilla Reinaldo <ghostcad@gmail.com>
2020-11-18 00:28:23 -0300

    actualizaciones para heroku 0.25

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 //Install express server
-var express = require('express');
-var path = require('path');
-var app = express();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/angularparcialwebhermosilla'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/angularparcialwebhermosilla/index.html'));
+
+app.get('/*', (req, res) => {
+
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
